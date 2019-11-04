1d6845e5d954ee325575bdb1a5fe2912fb26df4d
tainguyen2306 <48238014+tainguyen2306@users.noreply.github.com>
2019-11-04 23:38:46 +0700

    Add files via upload

--- flask/runserver.py
+++ flask/runserver.py
@@ -1,8 +1,9 @@
-# USAGE
+  # USAGE
 # python webstreaming.py --ip 0.0.0.0 --port 8000
 
 # import the necessary packages
 from imutils.video import VideoStream
+from imutils.video import FPS
 from flask import Flask, render_template, session, request, \
     copy_current_request_context, Response
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
@@ -19,7 +20,9 @@ import json
 from face import datagenerator, datatrainer, datarecognitior
 import base64
 import numpy as np
-from sensorlib import tempandhumldty as temphum
+from sensorlib import tempandhumldty as temphum, magiccup as magiccups, \
+    buttonled as button, FlameSensor as flameandbuzzer, ReedSwitchSensor as reedswitchled, TouchSensor as touchlaser, \
+    PIR as pir, Avoidance, Tracking, PhotoInterrupter as photointerrupter, Rotary
 
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful for multiple browsers/tabs
@@ -32,9 +35,12 @@ app = Flask(__name__)
 socketio = SocketIO(app, async_mode=async_mode)
 # initialize the video stream and allow the camera sensor to
 # warmup
-vs = VideoStream(usePiCamera=1).start()
-#vs = VideoStream(src=0).start()
+#vs = VideoStream(usePiCamera=True).start()
+vs = VideoStream(src=0).start()
 time.sleep(2.0)
+
+# start the FPS counter
+fps = FPS().start()
 count = None
 ids = None
 
@@ -45,6 +51,7 @@ def index():
 
 @socketio.on('connect')
 def connect():
+    datarecognitior.encodings()
     emit('sid_response', {'data': request.sid, 'count': 0})
 
 @app.route("/video_feed")
@@ -71,12 +78,168 @@ def recognition(sid):
 
 @socketio.on('tempandhumldty')
 def tempandhumldty():
-    while True:
+    temphum.Enabled = True
+    while temphum.Enabled:
         result = temphum.read_dht11_dat()
         if result:
             humidity, temperature = result
             emit('tempandhumldty_response', {'data': { 'humidity': humidity, 'temperature': temperature}, 'count': 0})
         time.sleep(1)
+
+@socketio.on('buttonled')
+def buttonledfunction(message):
+    if button.Enabled:
+        button.Enabled = False
+        button.sensorOff()
+    else:
+        button.Enabled = True
+        if "btnpin" in message:
+            button.BtnPin = message['btnpin']
+        if "ledpin" in message:
+            button.LedPin = message['ledpin']
+    button.turnOnOffLef()
+    emit('buttonled_response', {'data': { 'flagLed': button.Enabled}, 'count': 0})
+    
+@socketio.on('reedswitchled')
+def reedswitchledfunction(message):
+    if reedswitchled.Enabled:
+        reedswitchled.Enabled = False
+        reedswitchled.sensorOff()
+    else:
+        reedswitchled.Enabled = True
+        if "reedpin" in message:
+            reedswitchled.ReedPin = message['reedpin']
+        if "ledpin" in message:
+            reedswitchled.LedPin = message['ledpin']
+    reedswitchled.detectMagnetic()
+    emit('reedswitchled_response', {'data': { 'flagUse': reedswitchled.Enabled}, 'count': 0})
+    
+@socketio.on('touchlaser')
+def touchlaserfunction(message):
+    if touchlaser.Enabled:
+        touchlaser.Enabled = False
+        touchlaser.sensorOff()
+    else:
+        touchlaser.Enabled = True
+    emit('touchlaser_response', {'data': { 'flagUse': touchlaser.Enabled}, 'count': 0})
+    if "touchpin" in message:
+        touchlaser.TouchPin = message['touchpin']
+    if "laserpin" in message:
+        touchlaser.LazePin = message['laserpin']
+        touchlaser.touchAndLaser()
+ 
+@socketio.on('flameandbuzzer')
+def fireDetection(message):
+    flagFire = False
+    if flameandbuzzer.Enabled:
+        flameandbuzzer.Enabled = False
+        flameandbuzzer.sensorOff()
+        emit('flameandbuzzer_response', {'data': { 'flagFire': flagFire}, 'count': 0, 'enable': flameandbuzzer.Enabled})
+    else:
+        flameandbuzzer.Enabled = True
+    if "flamepin" in message:
+        flameandbuzzer.FlamePin = message['flamepin']
+    if "soundpin" in message:
+        flameandbuzzer.SoundPin = message['soundpin']
+    while flameandbuzzer.Enabled:
+        result = flameandbuzzer.fireDetection()
+        if result:
+            flagFire = result
+        emit('flameandbuzzer_response', {'data': { 'flagFire': flagFire}, 'count': 0, 'enable': flameandbuzzer.Enabled})
+        time.sleep(1)
+
+@socketio.on('magiccups')
+def magiccupsetup(message):
+
+    if magiccups.Enabled:
+        magiccups.Enabled = False
+        magiccups.sensorOff()
+    else:
+        magiccups.Enabled = True
+        if "signpin" in message:
+            magiccups.MERCURY_SIG1 = message['signpin']
+        if "ledpin" in message:
+            magiccups.LED1 = message['ledpin']
+    emit('magiccups_response', {'data': { 'flagMagiccups': magiccups.Enabled}, 'count': 0})
+    magiccups.magiccups()
+
+@socketio.on('rotarysetup')
+def rotarysetup(message):
+    if Rotary.Enabled:
+        Rotary.Enabled = False
+        Rotary.sensorOff()
+    else:
+        Rotary.Enabled = True
+        if "RoAPin" in message:
+            Rotary.RoAPin = message['RoAPin']
+        if "RoBPin" in message:
+            Rotary.RoBPin = message['RoBPin']
+        if "RoSPin" in message:
+            Rotary.RoSPin = message['RoSPin']
+        if "LedPin" in message:
+            Rotary.LedPin = message['LedPin']
+    emit('rotary_response', {'data': { 'flagRotary': Rotary.Enabled}, 'count': 0})
+    Rotary.setupRotary()
+
+@socketio.on('pirsensor')
+def pirsensor(message):
+
+    if pir.Enabled:
+        pir.Enabled = False
+        pir.sensorOff()
+    else:
+        pir.Enabled = True
+        if "PIRPin" in message:
+            pir.PIRPin = message['PIRPin']
+        if "SoundPin" in message:
+            pir.SoundPin = message['SoundPin']
+    emit('pirsensor_response', {'data': { 'flagPIR': pir.Enabled}, 'count': 0})
+    pir.PIRSetup()
+
+@socketio.on('avoidsensor')
+def avoidsensor(message):
+
+    if Avoidance.Enabled:
+        Avoidance.Enabled = False
+        Avoidance.sensorOff()
+    else:
+        Avoidance.Enabled = True
+        if "ObstaclePin" in message:
+            Avoidance.ObstaclePin = message['ObstaclePin']
+        if "ledpin" in message:
+            Avoidance.LedPin = message['ledpin']
+    emit('avoidsensor_response', {'data': { 'flagAvoid': Avoidance.Enabled}, 'count': 0})
+    Avoidance.AvoidSetup()
+
+@socketio.on('trackingsensor')
+def trackingsensor(message):
+
+    if Tracking.Enabled:
+        Tracking.Enabled = False
+        Tracking.sensorOff()
+    else:
+        Tracking.Enabled = True
+        if "TrackPin" in message:
+            Tracking.TrackPin = message['TrackPin']
+        if "ledpin" in message:
+            Tracking.LedPin = message['ledpin']
+    emit('trackingsensor_response', {'data': { 'flagTracking': Tracking.Enabled}, 'count': 0})
+    Tracking.TrackingSetup()
+
+@socketio.on('photointerrupter')
+def photointerruptersensor(message):
+
+    if photointerrupter.Enabled:
+        photointerrupter.Enabled = False
+        photointerrupter.sensorOff()
+    else:
+        photointerrupter.Enabled = True
+        if "Ky10Pin" in message:
+            photointerrupter.Ky10Pin = message['Ky10Pin']
+        if "ledpin" in message:
+            photointerrupter.LedPin = message['ledpin']
+    emit('photointerruptersensor_response', {'data': { 'flagPhotointerrupter': photointerrupter.Enabled}, 'count': 0})
+    photointerrupter.Ky10Setup()
 
 @socketio.on('persion_add')
 def persion_add(message):
@@ -119,10 +282,10 @@ def detect_motion(frameCount):
     while True:
         # read the next frame from the video stream, resize it,
         # convert the frame to grayscale, and blur it
+        # grab the frame from the threaded video stream and resize it
+        # to 500px (to speedup processing)
         frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        frame = imutils.resize(frame, width=500)
 
         # grab the current timestamp and draw it on the frame
         timestamp = datetime.datetime.now()
@@ -134,6 +297,8 @@ def detect_motion(frameCount):
         # lock
         with lock:
             outputFrame = frame.copy()
+
+    fps.stop()
         
 def generate():
     # grab global references to the output frame and lock variables
@@ -188,14 +353,15 @@ def generate_face(persion_id, sid):
             #wait for 'q' key was pressed and break from the loop
             if cv2.waitKey(1) & 0xff == ord("q"):
                 break
-            if count == 100:
+            if count == 10:
                 break
         # yield the output frame in the byte format
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
             bytearray(encodedImage) + b'\r\n')
-    datatrainer.training()
     socketio.emit('socket_generate_face_finish',
-             {'status': 'finish'}, room = sid)
+             {'status': 'finish'}, room = sid)      
+    datatrainer.training()
+    datarecognitior.encodings()
 
 def generate_recognition(sid):
     # grab global references to the output frame and lock variables
@@ -221,25 +387,23 @@ def generate_recognition(sid):
                 break
 
             if len(np.unique(ids)) > 0:
+                # yield the output frame in the byte format
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+                    bytearray(encodedImage) + b'\r\n')
                 break
 
         # yield the output frame in the byte format
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
             bytearray(encodedImage) + b'\r\n')
     rows = find_persion(ids)
-    print(rows)
-    print(len(rows))
-    print(len(ids))
     if len(rows) < len(ids):
         rows.append(("Khách", ""))
-    print(len(rows))
-    print(rows)
     jsondata = [{"first_name": x[0], "last_name": x[1]} for x in rows]
     socketio.emit('recognition_finish',
         {'data': jsondata, 'status': 'finish'}, room = sid)
 
 def find_persion(ids):
-
+    print(ids)
     con = sqlite3.connect("sensor.db")
     cur = con.cursor()
     sql="select first_name, last_name from people where id in ({seq})".format(
@@ -276,3 +440,5 @@ if __name__ == '__main__':
         destroy()
         # release the video stream pointer
         vs.stop()
+        # update the FPS counter
+        fps.stop()   
